a5d9c16b330e78b6187f58fad9290518c58c920c
Abdelkader-gnichi <ggnichi097@gmail.com>
2025-04-16 16:41:57 +0100

    FastAPI hello world example

---
+++ main.py
@@ -0,0 +1,15 @@
+from typing import Union
+
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def greeting():
+    return {'hello': 'world'}
+
+@app.get('/items/{item_id}/')
+def get_item_by_id(item_id: int, q: Union[str, None] = None):
+    return {'item_id:': item_id, 'q': q}
