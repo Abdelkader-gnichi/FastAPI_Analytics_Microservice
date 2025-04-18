799e1fbda72d03a01ac98b367a39da1f4ee8aa05
Abdelkader-gnichi <ggnichi097@gmail.com>
2025-04-18 16:42:31 +0100

    Basic Data types (api retrun data type validation

--- src/api/events/routing.py
+++ src/api/events/routing.py
@@ -1,7 +1,13 @@
 from fastapi import APIRouter
+from .schemas import EventSchema
 
 router = APIRouter()
 
 @router.get("/")
 def read_events():
     return {'items': [1,2,3]}
+
+
+@router.get("/{event_id}", response_model=EventSchema)
+def get_event_by_id(event_id: int): # or use -> EventSchema:
+    return {'id': event_id}
