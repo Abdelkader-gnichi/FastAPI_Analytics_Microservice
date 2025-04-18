06d263563043a270fbd90c54372b8040046789b8
Abdelkader-gnichi <ggnichi097@gmail.com>
2025-04-18 23:50:12 +0100

    local & docker compose Env Variables loading support

--- src/api/db/configs.py
+++ src/api/db/configs.py
@@ -1,3 +1,10 @@
 from decouple import config
 
-DATA_BASE_URL = config('DATA_BASE_URL', cast=str, default='')
+POSTGRES_USER = config('POSTGRES_USER', cast=str, default='')
+POSTGRES_PASSWORD = config('POSTGRES_PASSWORD', cast=str, default='')
+POSTGRES_DB = config('POSTGRES_DB', cast=str, default='')
+POSTGRES_HOST = config('POSTGRES_HOST', cast=str, default='')
+POSTGRES_PORT = config('POSTGRES_PORT', cast=str, default='')
+
+
+DATA_BASE_URL = f"postgresql+psycopg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
