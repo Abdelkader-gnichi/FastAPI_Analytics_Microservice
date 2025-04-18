b95ea1426dcb5646c04bd490f723694a00aa0456
Abdelkader-gnichi <ggnichi097@gmail.com>
2025-04-18 19:25:37 +0100

    Send Data to our API using POST Method

--- src/api/events/routing.py
+++ src/api/events/routing.py
@@ -16,3 +16,9 @@ def read_events() -> EventListSchema:
 @router.get("/{event_id}", response_model=EventSchema)
 def get_event_by_id(event_id: int): # or use -> EventSchema:
     return {'id': event_id}
+
+
+@router.post("/")
+def create_event(data: dict={}) -> EventSchema:
+    print(data)
+    return data
