f740a23f5809ffd5621a1f5fc0732b7c7d2cc6cf
Abdelkader-gnichi <ggnichi097@gmail.com>
2025-04-18 15:14:26 +0100

    setup a api health check endpoint

--- src/main.py
+++ src/main.py
@@ -13,3 +13,8 @@ def greeting():
 @app.get('/items/{item_id}/')
 def get_item_by_id(item_id: int, q: Union[str, None] = None):
     return {'item_id:': item_id, 'q': q}
+
+
+@app.get("/healthz")
+def get_health_status():
+    return {'status': 'ok'}
