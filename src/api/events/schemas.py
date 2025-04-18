f32c4933e0b9579c9e1e6c794b6429c87d9a9ab1
Abdelkader-gnichi <ggnichi097@gmail.com>
2025-04-18 17:11:08 +0100

    Basic Data types (api retrun data type validation; lists + int)

--- src/api/events/schemas.py
+++ src/api/events/schemas.py
@@ -1,6 +1,12 @@
 from pydantic import BaseModel
+from typing import List
 
 
 class EventSchema(BaseModel):
 
     id: int
+
+class EventListSchema(BaseModel):
+
+    results: List[EventSchema]
+    count: int
