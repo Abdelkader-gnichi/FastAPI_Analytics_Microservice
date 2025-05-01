c7efea8d050f99d27db26a6da327ed124ac622e7
abdelkader gnichi <ggnichi097@gmail.com>
2025-05-01 16:57:22 +0100

    configure Cors to handel external requestes

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from typing import Union
 from api.db.session import init_db
 from fastapi import FastAPI
 from api.events import router as events_router
+from fastapi.middleware.cors import CORSMiddleware
 
 
 @asynccontextmanager
@@ -14,7 +15,16 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
 app.include_router(events_router, prefix='/api/events')
+
 
 @app.get("/")
 def greeting():
